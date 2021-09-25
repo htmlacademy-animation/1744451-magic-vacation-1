fd5758c8e11416f87410cd6b562588d3aa693f66
Anton Mamkin <anton@mamkin.dev>
2021-09-25 13:55:52 +0300

    module3-task1
    3.13. Реализовываем анимацию иллюстрации второстепенного приза

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,3 +1,8 @@
+const updatePrizeSrc = (selector, imagePath) => {
+  const prizeEl = document.querySelector(selector);
+  prizeEl.querySelector(`img`).setAttribute(`src`, imagePath);
+};
+
 export default () => {
   let header = document.querySelector(`.js-header`);
   let menuToggler = document.querySelector(`.js-menu-toggler`);
@@ -22,8 +27,8 @@ export default () => {
         document.body.classList.remove(`menu-opened`);
       }
 
-      const firstPrize = document.querySelector(`.prizes__item--journeys`);
-      firstPrize.querySelector(`img`).setAttribute(`src`, `img/module-3/img/primary-award.svg`);
+      updatePrizeSrc(`.prizes__item--journeys`, `img/module-3/img/primary-award.svg`);
+      updatePrizeSrc(`.prizes__item--cases`, `img/second-award.svg`);
     });
   }
 };
