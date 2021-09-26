9fa567ffaa0c83c0c71b49291aea29e3455919cb
Anton Mamkin <anton@mamkin.dev>
2021-09-26 19:59:24 +0300

    fix prizes animation

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -61,12 +61,18 @@ const prepareLetters = (textNode) => {
   }
 };
 
+const delay = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const primaryAward = document.getElementById(`primary-award`);
 const secondaryAward = document.getElementById(`secondary-award`);
 const additionalAward = document.getElementById(`additional-award`);
 
-export const startPrizesAnimation = () => {
+export const startPrizesAnimation = async () => {
   primaryAward.src = `img/primary-award.svg?${new Date().getTime()}`;
+  await delay(4000);
   secondaryAward.src = `img/second-award.svg?${new Date().getTime()}`;
+  await delay(2500);
   additionalAward.src = `img/additional-award.svg?${new Date().getTime()}`;
 };
